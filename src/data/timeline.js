0ba57336af04d1b6f78a4e888e2f90ab87d10113
Gunal C <gunalcsg0087@gmail.com>
2026-08-01 22:43:07 +0530

    resume updation

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,10 +1,10 @@
 export const timeline = [
   {
-    year: '2020',
+    year: '2026',
     type: 'Education',
-    title: 'SSLC',
+    title: 'Bachelor of Engineering',
     description:
-      'Completed my SSLC at Model School with 69%, building a strong academic foundation.'
+      'Graduated with a B.E. in Computer Science and Engineering from Kongu Engineering College with a CGPA of 7.49. This journey strengthened my problem-solving skills and passion for software development.'
   },
   {
     year: '2022',
@@ -14,10 +14,10 @@ export const timeline = [
       'Completed my Higher Secondary Certificate (HSC) with 82.5%, strengthening my interest in computer science and technology.'
   },
   {
-    year: '2026',
+    year: '2020',
     type: 'Education',
-    title: 'Bachelor of Engineering',
+    title: 'SSLC',
     description:
-      'Graduated with a B.E. in Computer Science and Engineering from Kongu Engineering College with a CGPA of 7.49. This journey strengthened my problem-solving skills and passion for software development.'
+      'Completed my SSLC at Model School with 69%, building a strong academic foundation.'
   }
 ];
